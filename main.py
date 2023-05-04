7aa0169cd33588fcdfb164e09f336e1d6ea1effe
Ivan Pavlyukevich <serpavminsk59@gmail.com>
2023-05-04 16:38:58 +0300

    Lab1 : ready Lab2 : needs some work Lab3 : not ready Lab4 : not ready

---
+++ main.py
@@ -0,0 +1,104 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.uix.image import AsyncImage
+from kivy.properties import ObjectProperty
+import os
+from striprtf.striprtf import rtf_to_text
+from process_file import ProcessFile
+from kivy.graphics.svg import Svg
+
+
+class MyWidget(BoxLayout):
+    input_widget = ObjectProperty(None)
+    output_widget = ObjectProperty(None)
+    filter_word_widget = ObjectProperty(None)
+
+    def open_window(self):
+        _popup = DownloadInfo(self.input_widget)
+        _popup.open()
+
+    def save_window(self):
+        _popup = SaveInfo(self.output_widget)
+        _popup.open()
+
+    def process_file(self):
+        PrF = ProcessFile(self.input_widget.text)
+        dict1 = PrF.result()
+        self.output_widget.text = dict1
+
+    def treeify_file(self):
+        PrF = ProcessFile(self.input_widget.text)
+        PrF.dep_trees()
+        _Popup = ImageP('photo.svg')
+        _Popup.open()
+
+    def filter_words(self):
+        PrF = ProcessFile(self.input_widget.text)
+        PrF.result()
+        self.output_widget.text = PrF.filter_results(self.filter_word_widget.text)
+
+    def about(self):
+        _AboutPage = Info()
+        _AboutPage.open()
+
+
+class DownloadInfo(Popup):
+    def __init__(self, *args, **kwargs):
+        super(DownloadInfo, self).__init__(**kwargs)
+        self.input_widget = args[0]
+
+    def close_window(self):
+        self.dismiss()
+
+    def select_file(self, path):
+        path = ''.join(path)
+        filename, ext = os.path.splitext(path)
+        if ext == ".txt":
+            with open(path, encoding='utf-8') as stream:
+                self.input_widget.text = stream.read()
+        elif ext == ".rtf":
+            with open(path, encoding='utf-8') as stream:
+                rtf = stream.read()
+                self.input_widget.text = rtf_to_text(rtf)
+        self.close_window()
+
+
+class SaveInfo(Popup):
+    file_name = ObjectProperty('Input1')
+    path = "output"
+
+    def __init__(self, *args, **kwargs):
+        super(SaveInfo, self).__init__(**kwargs)
+        self.output_widget = args[0]
+
+    def close_window(self):
+        self.dismiss()
+
+    def save_file(self):
+        full_dir = os.path.join(self.path, self.file_name.text)
+        with open(full_dir, "w",  encoding='utf-8') as stream:
+            stream.write(self.output_widget.text)
+        self.output_widget.text = ""
+        self.close_window()
+
+
+class Info(Popup):
+    pass
+
+
+class ImageP(Popup):
+    def __init__(self, *args, **kwargs):
+        super(ImageP, self).__init__(**kwargs)
+        source = args[0]
+        self.ids.my_img.source = source
+
+
+class MyApp(App):
+    def build(self):
+        return MyWidget()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
---
+++ process_file.py
@@ -0,0 +1,69 @@
+import spacy
+from spacy import displacy
+from pathlib import Path
+import time
+
+
+class ProcessFile:
+    def __init__(self, input_text):
+        self.nlp = spacy.load("en_core_web_sm")
+        time_start = time.time()
+        self.input = self.nlp(input_text)
+        self.my_dict = {}
+        time_end = time.time()
+        print("Time for processing: " + str(time_end-time_start))
+
+    def result(self):
+        for i in self.input:
+            if i.text not in [',', ':', '!', '?', '.', ';', "'", "\n", "–", "‘", " "]:
+                self.my_dict[i.text] = f"lexeme: {i.lemma_}, pos: {i.pos_}, morphological_features: {i.morph}, " \
+                                       f"possible_role: {str(self.role_of_word_in_sentence(i))}\n"
+        str1 = self.beauty_result(self.my_dict)
+        print("Word count: " + str(len(self.input)))
+        print("Unique lexeme count: " + str(len(self.my_dict.keys())))
+        return str1
+
+    def filter_results(self, filter_word):
+        new_dict = {}
+        if filter_word == '':
+            new_dict = dict(self.my_dict)
+        for key, value in self.my_dict.items():
+            if key == filter_word:
+                new_dict[key] = value
+        if not new_dict:
+            return "The word in the filter is not in the text!"
+        else:
+            return self.beauty_result(new_dict)
+
+    def beauty_result(self, ugly_dict):
+        str_result = ''
+        for key, i in ugly_dict.items():
+            str_result = str_result + (key + "\n\t" + str(i) + '\n\n')
+
+        return str_result
+
+    def role_of_word_in_sentence(self, token) -> list:
+        role_of_word = []
+        if token.pos_ in ("NOUN", "PROPN", "PRON", "NUM"):
+            role_of_word.append("Subject")
+        if token.pos_ in ("AUX", "VERB"):
+            role_of_word.append("Predicate")
+        if token.pos_ in ("VERB", "PROPN", "PRON", "NOUN", "ADJ", "NUM"):
+            role_of_word.append("Object")
+        if token.pos_ in ("ADJ"):
+            role_of_word.append("Attribute")
+        if token.pos_ in ("ADV"):
+            role_of_word.append("Adverbial Modifier")
+        if not role_of_word:
+            role_of_word.append("Not detected")
+        return role_of_word
+
+    def dep_trees(self):
+        if self.input:
+            dependency = displacy.render(self.input, style="dep", jupyter=False)
+            file_name = "photo.svg"
+            output_path = Path(file_name)
+            output_path.open("w", encoding="utf-8").write(dependency)
+            return dependency
+        else:
+            print("Please choose a file.")
